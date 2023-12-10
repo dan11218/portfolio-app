55974eeeb670fcdf013aecda1f2051a3e2ad9f99
Danny Graugnard <dangraug@me.com>
2023-12-10 11:24:24 -0500

    adds the view prototype button

--- app/jumbotron.tsx
+++ app/jumbotron.tsx
@@ -12,7 +12,7 @@ export default function Jumbotron() {
                 <div className="flex flex-col justify-center w-[500px] px-[50px]">
                     <h2 className="font-semibold text-4xl py-[5px]">Hi, I&apos;m Danny! 👋</h2>
                     <h3 className="font-medium text-xl py-[20px] text-sky-700">I&apos;m a designer with a background in Software Engineering and Customer Support</h3>
-                    <Link href="https://drive.google.com/file/d/1nTI56A3wOYprQhMZaEhc4_X8DhuG4c_5/view?usp=sharing" className="w-fit h-10 px-5 flex items-center font-regular text-xs text-slate-50 transition-colors duration-150 bg-slate-800 rounded-lg focus:shadow-outline hover:bg-slate-600">Download CV</Link>
+                    <Link href="https://drive.google.com/file/d/1nTI56A3wOYprQhMZaEhc4_X8DhuG4c_5/view?usp=sharing" className="w-fit h-10 px-5 flex items-center font-medium text-sm text-slate-50 transition-colors duration-150 bg-slate-800 rounded-lg focus:shadow-outline hover:bg-slate-600">Download CV</Link>
                 </div>
             </div>
         </div>
--- app/project.tsx
+++ app/project.tsx
@@ -6,13 +6,16 @@ export default function Project(props: any) {
         <div className="px-[200px] py-[50px] justify-center inline-flex w-full">
             <div className={`justify-between inline-flex w-full max-w-6xl ${props.alignRight ? "flex-row-reverse": []} `}>
                 <div className="flex justify-center">
-                    <img src={props.image_url} width={420} height={420} alt="project thumbnail" />
+                    <Image src={props.image_url} width={420} height={420} alt="project thumbnail" />
                 </div>
-                <div className="flex flex-col justify-center px-[50px] w-[600px]">
+                <div className="flex flex-col px-[50px] w-[600px]">
                     <h4 className="font-medium text-sm py-[5px] text-slate-600">{props.skills}</h4>
                     <h2 className="font-semibold text-3xl py-[5px] text-slate-800">{props.title}</h2>
                     <p className="font-regular text-sm py-[20px] text-slate-800">{props.description}</p>
-                    <Link href={props.case_study_url} className="w-fit flex items-center h-10 px-5 font-regular text-xs text-slate-50 transition-colors duration-150 bg-slate-800 rounded-lg focus:shadow-outline hover:bg-slate-600">See Case Study</Link>
+                    <div className="flex flex-row justify-between w-80">
+                        <Link href={props.case_study_url} className="w-fit flex items-center h-10 px-5 font-medium text-sm text-slate-50 transition-colors duration-150 bg-slate-800 rounded-lg focus:shadow-outline hover:bg-slate-600">See Case Study</Link>
+                        <Link href={props.case_study_url} className="w-fit flex items-center h-10 px-5 font-medium text-sm text-slate-50 transition-colors duration-150 bg-slate-800 rounded-lg focus:shadow-outline hover:bg-slate-600">View Prototype</Link>
+                    </div>
                 </div>
             </div>
         </div>
