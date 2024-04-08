78046ebddaa998e02c417470ea7df3989b297e69
Danny Graugnard <dangraug@me.com>
2024-04-08 10:13:36 -0400

    adds new images, fixes alignment

--- app/projects/gift-shop-demo/final-designs/finalDesign.tsx
+++ app/projects/gift-shop-demo/final-designs/finalDesign.tsx
@@ -10,102 +10,118 @@ export default function FinalDesign() {
           <hr className="m-auto my-1 mb-16 h-1 w-[150px] rounded bg-gradient-to-r from-sky-500 to-cyan-300 dark:bg-gray-300" />
         </div>
         <div className="flex w-full flex-col space-y-16">
-          <div>
+          <div className="flex flex-col items-center md:items-start">
             <h3 className="mb-4">Add to cart</h3>
-            <div id="shop flow" className="flex flex-wrap items-start">
+            <div
+              id="shop flow"
+              className="flex flex-wrap items-start justify-center md:justify-normal"
+            >
               <Image
                 src="/gift-shop-demo/screens/Home-page.png"
                 width={300}
                 height={300}
                 alt="Home page"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
               />
               <Image
                 src="/gift-shop-demo/screens/Product-research.png"
                 width={300}
                 height={300}
                 alt="Shop page"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
               />
               <Image
                 src="/gift-shop-demo/screens/Product-page.png"
                 width={300}
                 height={300}
                 alt="Product page"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
               />
               <Image
                 src="/gift-shop-demo/screens/Cart.png"
                 width={300}
                 height={300}
                 alt="Cart page"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
               />
               <Image
                 src="/gift-shop-demo/screens/Cart-campaign.png"
                 width={300}
                 height={300}
                 alt="cart with modal"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
               />
             </div>
           </div>
-          <div>
-            <h3 className="mb-4">Checkout</h3>
-            <div id="new-event-screens" className="flex flex-wrap items-start">
+          <div className="flex flex-col items-center md:items-start">
+            <h3 className="mb-4">Checkout with survey</h3>
+            <div
+              id="new-event-screens"
+              className="flex flex-wrap items-start justify-center md:justify-normal"
+            >
               <Image
                 src="/gift-shop-demo/screens/Payment.png"
                 width={300}
                 height={300}
                 alt="new event form"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
               />
               <Image
                 src="/gift-shop-demo/screens/Payment-Delivery-Options.png"
                 width={300}
                 height={300}
                 alt="list of vendors (filtered)"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
               />
               <Image
                 src="/gift-shop-demo/screens/Payment-Pay.png"
                 width={300}
                 height={300}
                 alt="list of vendors (filtered)"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
               />
               <Image
                 src="/gift-shop-demo/screens/Payment-Campaign.png"
                 width={300}
                 height={300}
                 alt="list of vendors (filtered)"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
               />
               <Image
                 src="/gift-shop-demo/screens/Post-payment.png"
                 width={300}
                 height={300}
                 alt="list of vendors (filtered)"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
+              />
+              <Image
+                src="/gift-shop-demo/screens/Post-payment-Campaign.png"
+                width={300}
+                height={300}
+                alt="list of vendors (filtered)"
+                className="mb-4 mr-4 shadow-lg"
               />
             </div>
           </div>
-          <div>
+          <div className="flex flex-col items-center md:items-start">
             <h3 className="mb-4">Registration</h3>
-            <div id="new-event-screens" className="flex flex-wrap items-start">
+            <div
+              id="new-event-screens"
+              className="flex flex-wrap items-start justify-center md:justify-normal"
+            >
               <Image
                 src="/gift-shop-demo/screens/Checkout.png"
                 width={300}
                 height={300}
                 alt="new event form"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
               />
               <Image
                 src="/gift-shop-demo/screens/Checkout-campaign.png"
                 width={300}
                 height={300}
                 alt="list of vendors (filtered)"
-                className="mb-2 mr-4 shadow-lg"
+                className="mb-4 mr-4 shadow-lg"
               />
             </div>
           </div>
