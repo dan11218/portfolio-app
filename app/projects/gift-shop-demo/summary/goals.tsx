12df6145205423fdf6074470dfe5cd05b54c00b4
Danny Graugnard <dangraug@me.com>
2024-05-07 22:48:11 -0400

    ''

--- app/projects/gift-shop-demo/summary/goals.tsx
+++ app/projects/gift-shop-demo/summary/goals.tsx
@@ -2,14 +2,14 @@ export default function Goals() {
   return (
     <div className="flex flex-col">
       <h3 className="mb-3 font-bold text-cyan-500">{`Goals`}</h3>
-      <ul className="list-disc px-8 text-sm">
+      <ul className="list-disc px-2 text-sm">
         <li>
           <strong>{`Business: `}</strong>
           {`Design an e-commerce experience that Sales team and Solutions Engineers to simulate the Feedback Button and targeted campaigns.`}
         </li>
         <li>
           <strong>{`Users: `}</strong>
-          {`create a platform that is accessible for freelance event planners, tapping into a niche market.`}
+          {`create a gift shop where users can shop for usabilla swag. Users can leave passive feedback using the Feedback Button, or receive targeted surveys to collect feedback during the user journey.`}
         </li>
       </ul>
     </div>
--- app/projects/plannit/summary/goals.tsx
+++ app/projects/plannit/summary/goals.tsx
@@ -2,7 +2,7 @@ export default function Goals() {
   return (
     <div className="flex flex-col">
       <h3 className="mb-3 font-bold text-rose-400">Goals</h3>
-      <ul className="list-disc px-8 text-sm">
+      <ul className="list-disc px-2 text-sm">
         <li>
           <strong>{`Business: `}</strong>
           {`create a platform that is accessible for freelance event planners and vendors, tapping into a potential new market.`}
