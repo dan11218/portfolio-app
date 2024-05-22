62717fb3ba4975c7db864b173b68a9a9563bc34f
Danny Graugnard <dangraug@me.com>
2024-05-22 13:34:00 -0400

    ''

--- app/projects/gift-shop-demo/design-process/design-requirements.tsx
+++ app/projects/gift-shop-demo/design-process/design-requirements.tsx
@@ -3,18 +3,18 @@ export default function DesignRequirements() {
     <div className="mb-16 flex w-full max-w-xl flex-col">
       <h3 className="mb-3 font-bold text-cyan-500">Design Requirements</h3>
       <ul className="list-disc px-6">
-        <li>Implement the Usabilla Feedback Button</li>
+        <li>{`Implement the Usabilla Feedback Button`}</li>
         <li>
-          Trigger a survey when the user successfully completes the checkout
-          flow
+          {`Trigger a survey when the user successfully completes the checkout
+          flow`}
         </li>
         <li>
-          Trigger a survey when a user experiences an error during the checkout
-          flow
+          {`Trigger a survey when a user experiences an error during the checkout
+          flow`}
         </li>
         <li>
-          Utilize user personas to design the checkout flow and trigger surveys
-          based on happy/sad paths
+          {`Utilize user personas to design the checkout flow and trigger surveys
+          based on happy/sad paths`}
         </li>
       </ul>
     </div>
