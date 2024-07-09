e201b0da3c124ca5a31a257cf7d9e0fce9e4dab8
Danny Graugnard <dangraug@me.com>
2024-07-09 13:35:44 -0400

    fixes image file names

--- app/projects/plannit/final-designs/sign-up.tsx
+++ app/projects/plannit/final-designs/sign-up.tsx
@@ -6,56 +6,56 @@ export default function SignUp() {
       <h3 className="mb-4">Sign-up</h3>
       <div id="sign-up-screens" className="flex h-full w-full flex-wrap">
         <Image
-          src="/static/plannit/login/welcome screen.png"
+          src="/static/plannit/login/welcome_screen.png"
           width={185}
           height={100}
           alt="registration"
           className="mb-4 mr-8 shadow-lg"
         />
         <Image
-          src="/static/plannit/login/sign up - deselected.png"
+          src="/static/plannit/login/sign_up_deselected.png"
           width={185}
           height={100}
           alt="choose profile type"
           className="mb-4 mr-8 shadow-lg"
         />
         <Image
-          src="/static/plannit/login/sign up - planner.png"
+          src="/static/plannit/login/sign_up_planner.png"
           width={185}
           height={100}
           alt="profile selected"
           className="mb-4 mr-8 shadow-lg"
         />
         <Image
-          src="/static/plannit/login/sign up - vendor.png"
+          src="/static/plannit/login/sign_up_vendor.png"
           width={185}
           height={100}
           alt="create a profile"
           className="mb-4 mr-8 shadow-lg"
         />
         <Image
-          src="/static/plannit/login/create profile.png"
+          src="/static/plannit/login/create_profile.png"
           width={185}
           height={100}
           alt="welcome screen"
           className="mb-4 mr-8 shadow-lg"
         />
         <Image
-          src="/static/plannit/login/create profile - populated.png"
+          src="/static/plannit/login/create_profile_populated.png"
           width={185}
           height={100}
           alt="welcome screen"
           className="mb-4 mr-8 shadow-lg"
         />
         <Image
-          src="/static/plannit/login/create profile - errors.png"
+          src="/static/plannit/login/create_profile_errors.png"
           width={185}
           height={100}
           alt="errors in fields"
           className="mb-4 mr-8 shadow-lg"
         />
         <Image
-          src="/static/plannit/login/today view.png"
+          src="/static/plannit/login/today_view.png"
           width={185}
           height={100}
           alt="today view"
