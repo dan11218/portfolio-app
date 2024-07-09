1138a00b4445b9b928ed746c35edc28cd50887fd
Danny Graugnard <dangraug@me.com>
2024-07-09 15:10:34 -0400

    makes images responsive

--- app/projects/plannit/final-designs/sign-up.tsx
+++ app/projects/plannit/final-designs/sign-up.tsx
@@ -5,69 +5,87 @@ export default function SignUp() {
     <div className="flex w-full max-w-7xl flex-col">
       <h3 className="mb-4">Sign-up</h3>
       <div id="sign-up-screens" className="flex h-full w-full flex-wrap">
-        <Image
-          src="/static/plannit/login/welcome_screen.png"
-          width={185}
-          height={100}
-          alt="registration"
-          className="mb-4 mr-8 shadow-lg"
-        />
-        <Image
-          src="/static/plannit/login/sign_up_deselected.png"
-          width={185}
-          height={100}
-          alt="choose profile type"
-          className="mb-4 mr-8 shadow-lg"
-        />
-        <Image
-          src="/static/plannit/login/sign_up_planner.png"
-          width={185}
-          height={100}
-          alt="profile selected"
-          className="mb-4 mr-8 shadow-lg"
-        />
-        <Image
-          src="/static/plannit/login/sign_up_vendor.png"
-          width={185}
-          height={100}
-          alt="create a profile"
-          className="mb-4 mr-8 shadow-lg"
-        />
-        <Image
-          src="/static/plannit/login/create_profile.png"
-          width={185}
-          height={100}
-          alt="create profile"
-          className="mb-4 mr-8 shadow-lg"
-        />
-        <Image
-          src="/static/plannit/login/create_profile_populated.png"
-          width={185}
-          height={100}
-          alt="create profile populated"
-          className="mb-4 mr-8 shadow-lg"
-        />
-        <Image
-          src="/static/plannit/login/create_profile_errors.png"
-          width={185}
-          height={100}
-          alt="errors in fields"
-          className="mb-4 mr-8 shadow-lg"
-        />
-        <Image
-          src="/static/plannit/login/today_view.png"
-          width={185}
-          height={100}
-          alt="today view"
-          className="mb-4 mr-8 shadow-lg"
-        />
-        <Image
-          src="/static/plannit/login/calendar.png"
-          width={185}
-          height={100}
-          alt="calendar"
-          className="mb-4 mr-8 shadow-lg"
-        />
+        <div className={`mb-8 mr-8 flex w-full max-w-[200px] flex-col`}>
+          <Image
+            src="/static/plannit/login/welcome_screen.png"
+            width={500}
+            height={500}
+            alt="registration"
+            className="mb-4 mr-8 shadow-lg"
+          />
+        </div>
+        <div className={`mb-8 mr-8 flex w-full max-w-[200px] flex-col`}>
+          <Image
+            src="/static/plannit/login/sign_up_deselected.png"
+            width={500}
+            height={500}
+            alt="choose profile type"
+            className="mb-4 mr-8 shadow-lg"
+          />
+        </div>
+        <div className={`mb-8 mr-8 flex w-full max-w-[200px] flex-col`}>
+          <Image
+            src="/static/plannit/login/sign_up_planner.png"
+            width={500}
+            height={500}
+            alt="profile selected"
+            className="mb-4 mr-8 shadow-lg"
+          />
+        </div>
+        <div className={`mb-8 mr-8 flex w-full max-w-[200px] flex-col`}>
+          <Image
+            src="/static/plannit/login/sign_up_vendor.png"
+            width={500}
+            height={500}
+            alt="create a profile"
+            className="mb-4 mr-8 shadow-lg"
+          />
+        </div>
+        <div className={`mb-8 mr-8 flex w-full max-w-[200px] flex-col`}>
+          <Image
+            src="/static/plannit/login/create_profile.png"
+            width={500}
+            height={500}
+            alt="create profile"
+            className="mb-4 mr-8 shadow-lg"
+          />
+        </div>
+        <div className={`mb-8 mr-8 flex w-full max-w-[200px] flex-col`}>
+          <Image
+            src="/static/plannit/login/create_profile_populated.png"
+            width={500}
+            height={500}
+            alt="create profile populated"
+            className="mb-4 mr-8 shadow-lg"
+          />
+        </div>
+        <div className={`mb-8 mr-8 flex w-full max-w-[200px] flex-col`}>
+          <Image
+            src="/static/plannit/login/create_profile_errors.png"
+            width={500}
+            height={500}
+            alt="errors in fields"
+            className="mb-4 mr-8 shadow-lg"
+          />
+        </div>
+        <div className={`mb-8 mr-8 flex w-full max-w-[200px] flex-col`}>
+          <Image
+            src="/static/plannit/login/today_view.png"
+            width={500}
+            height={100}
+            alt="today view"
+            className="mb-4 mr-8 shadow-lg"
+          />
+        </div>
+        <div className={`mb-8 mr-8 flex w-full max-w-[200px] flex-col`}>
+          <Image
+            src="/static/plannit/login/calendar.png"
+            width={500}
+            height={100}
+            alt="calendar"
+            className="mb-4 mr-8 shadow-lg"
+          />
+        </div>
       </div>
     </div>
   );
