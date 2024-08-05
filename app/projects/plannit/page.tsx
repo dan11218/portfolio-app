ec69d57e8f100308f0bfdba6ef1ec625b5c5fd6d
Danny Graugnard <dangraug@me.com>
2024-08-05 18:19:25 -0400

    moves this section higher

--- app/projects/plannit/page.tsx
+++ app/projects/plannit/page.tsx
@@ -3,15 +3,16 @@
 import Design from "./design/design";
 import FinalDesign from "./final-designs/finalDesign";
 import Hero from "./hero";
-import Overview from "./overview/overview";
 import Research from "./research/research";
 import Highlights from "./summary/highlights";
 import Summary from "./summary/summary";
+import Introduction from "./summary/introduction";
 
 export default async function Plannit() {
   return (
     <main className="w-screen flex-col">
       <Hero />
+      <Introduction />
       <Summary />
       <Highlights />
       <Research />
--- app/projects/plannit/summary/highlights.tsx
+++ app/projects/plannit/summary/highlights.tsx
@@ -4,18 +4,6 @@ import Image from "next/image";
 export default function Highlights() {
   return (
     <section className={`flex h-full w-full flex-col pb-24`}>
-      <div className="flex w-full flex-col items-center bg-gradient-to-l from-rose-100 to-orange-100 px-8 py-36 dark:bg-stone-800">
-        <div className={`flex w-full max-w-4xl flex-col items-center`}>
-          <h2 className={`${inter.className}`}>
-            <span className="text-rose-500">{`Plannit`}</span>
-            {` is an all-in-one event management platform designed to `}
-            <span className="text-rose-500">{`organize tasks`}</span>{" "}
-            {` and connect you with a vast network of `}
-            <span className="text-rose-500">{`professional vendors`}</span>
-            {` to create the perfect event.`}
-          </h2>
-        </div>
-      </div>
       <div className="flex flex-col items-center">
         <div className="flex w-full max-w-6xl flex-col items-center justify-between px-8 py-16 md:flex-row ">
           <div
