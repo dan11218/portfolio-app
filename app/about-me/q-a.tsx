90e7a4fc2935f022df984c891d2a5e5db62642d6
Danny Graugnard <dangraug@me.com>
2024-08-19 20:22:15 -0400

    adds q and a section

--- app/about-me/q-a.tsx
+++ app/about-me/q-a.tsx
@@ -1,7 +1,7 @@
 export default function QA() {
   return (
     <div className={`flex flex-col py-16`}>
-      <h2 className="mb-8">{`Q&A`}</h2>
+      <h2 className="mb-8">{`Q&A 🎤`}</h2>
       <div className="mb-8 flex flex-col">
         <h3>{`I want to work for a company that...`}</h3>
         <p>{`Values a teammate's professional growth, and communicates their mission, goals, and strategies with the team.`}</p>
