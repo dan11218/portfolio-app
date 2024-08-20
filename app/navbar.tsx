f9e3d532f01472729968764eaf487a5301f8b2b4
Danny Graugnard <dangraug@me.com>
2024-08-20 11:22:02 -0400

    removes logging

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -42,7 +42,6 @@ export default function Navbar() {
   };
 
   const collapseNavbar = (event: MouseEvent | TouchEvent) => {
-    console.log(ref.current);
     if (ref.current && !ref.current.contains(event.target as Node)) {
       setToggle(false);
     }
