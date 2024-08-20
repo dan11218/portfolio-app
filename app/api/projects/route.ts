d541e2d8ff6891bed60f5f28f7add68abc6df459
Danny Graugnard <dangraug@me.com>
2024-08-20 10:58:49 -0400

    fixes file path

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import projects from "@/app/project_data.json";
+import projects from "../../project_data.json";
 
 export async function GET() {
   return NextResponse.json({ projects });
