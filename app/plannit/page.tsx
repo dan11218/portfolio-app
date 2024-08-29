865137c21de23e75954f2b8ebf1015cee927639c
Danny Graugnard <dangraug@me.com>
2024-08-29 16:34:39 -0400

    updates bg colors

--- app/plannit/page.tsx
+++ app/plannit/page.tsx
@@ -9,7 +9,7 @@ import Introduction from "./overview/introduction";
 
 export default async function Plannit() {
   return (
-    <div className="w-screen flex-col bg-[#FFFAF0] dark:bg-stone-800">
+    <div className="w-screen flex-col bg-[#FCFDFC] dark:bg-stone-800">
       <Hero />
       <Introduction />
       <Overview />
